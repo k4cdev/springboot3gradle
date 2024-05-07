490436a9776f373b09140b9ac297a3563ef2682c
k4cdev <kevinoliverosjaraba@gmail.com>
2024-05-07 18:05:03 -0500

    Add authentication manager for login

--- demo/src/main/java/com/springboot3/demo/auth/AuthService.java
+++ demo/src/main/java/com/springboot3/demo/auth/AuthService.java
@@ -5,6 +5,9 @@ import com.springboot3.demo.user.Role;
 import com.springboot3.demo.user.User;
 import com.springboot3.demo.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +18,13 @@ public class AuthService {
     private final UserRepository userRepository;
     private final JwtService jwtService;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     AuthResponse login(LoginRequest login){
-        return null;
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(login.getUsername(), login.getPassword()));
+        UserDetails user = userRepository.findByUsername(login.getUsername()).orElseThrow();
+        String token = jwtService.getToken(user);
+        return AuthResponse.builder().token(token).build();
     }
 
     AuthResponse register(RegisterRequest register){
